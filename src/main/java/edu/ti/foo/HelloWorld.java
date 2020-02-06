c27a049fce1f1c642e8614b657f586aad5c4e1fe
cliberman <chanaliberman1@gmail.com>
2020-02-05 21:41:50 -0600

    added command line argument processing

--- src/main/java/edu/ti/foo/HelloWorld.java
+++ src/main/java/edu/ti/foo/HelloWorld.java
@@ -2,6 +2,10 @@ package edu.ti.foo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String name = "Bob";
+        if (args.length > 0) {
+            name = args[0];
+        }
+        System.out.println("Hello, " + name + " from the world!");
     }
 }
