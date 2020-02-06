7eeca0b5f651c8ad0e76835f68b927bbb18f27bc
cliberman <chanaliberman1@gmail.com>
2020-02-05 21:47:16 -0600

    cooler command line processing

--- src/main/java/edu/ti/foo/HelloWorld.java
+++ src/main/java/edu/ti/foo/HelloWorld.java
@@ -2,10 +2,7 @@ package edu.ti.foo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String name = "Bob";
-        if (args.length > 0) {
-            name = args[0];
-        }
+        String name = (args.length > 0) ? args[0] : "Bob";
         System.out.println("Hello, " + name + " from the world!");
     }
 }
